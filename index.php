be69b9e62be2561531480d62d48acd33a634a226
Renan Teotonio <renantn93@gmail.com>
2021-02-16 09:23:37 -0400

    refatorando e organizando o código

--- index.php
+++ index.php
@@ -7,19 +7,11 @@ $url = (isset($_GET['url'])) ? $_GET['url'] : 'index.php';
  *  sendo a $key a url informada
  *  e o $value o caminho do arquivo
  */
-
-
 $router = [
     'teste/' => 'Controllers/TesteControllers.php',
     'contato/' => 'Controllers/ContatoController.php',
     'models/teste/' => 'Models/Teste.php'
 ];
 
-
 //verifica se o url informada tem uma rota definida se tiver chama a rota se não chama rota não encontrada
 array_key_exists ( $url ,$router ) ? require $router[$url] : require "404.php";
-
-
-
-//se não encontrar a rota permanece no index.php
-//echo "index.php";
