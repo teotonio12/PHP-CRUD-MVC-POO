cfff8f50ae6d62b642351c4684426cc4af16114b
Renan Teotonio <renantn93@gmail.com>
2021-02-16 07:55:07 -0400

    initial commit

---
+++ Controllers/ContatoController.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "ContatoController.php";
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+//pega a rota informa, caso não tenha sido set o index.php
+$url = (isset($_GET['url'])) ? $_GET['url'] : 'index.php';
+
+/** define todas as rotas 
+ *  sendo a $key a url informada
+ *  e o $value o caminho do arquivo
+ */
+$router = array(
+    'teste/' => 'Controllers/TesteControllers.php',
+    'contato/' => 'Controllers/ContatoController.php',
+    'Models/Teste/' => 'Models/teste.php',
+    'TesteView/' => 'Resource/View/TesteView.php'
+);
+
+//passa por todas as rotas
+foreach ($router as $key => $value) {
+    
+    //verifica se a rota existe se sim finaliza a busca
+    if ($key == $url) {
+        require $router[$url];
+        die;
+    }
+
+}
+
+//se não encontrar a rota permanece no index.php
+echo "index.php";
